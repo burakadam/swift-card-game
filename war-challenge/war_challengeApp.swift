a3363a666e4a8df89359342fd4389bb21165543e
burakadam <buraksahinerden@gmail.com>
2022-08-02 14:20:59 +0300

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Burak Şahin Erden on 2.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
